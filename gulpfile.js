83abd0722296cedadf2bc118259a2baf0c3aeda5
Maurício Mutte <litiobr@gmail.com>
2018-12-14 14:28:00 -0200

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const cssmin = require('gulp-cssmin');
+const stripCssComments = require('gulp-strip-css-comments');
+const watch = require('gulp-watch');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+
+gulp.task('server', function(){
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch(['assets/css/style.css'],['css'])
+});
+
+gulp.task('css', function(){
+  gulp.src('assets/css/style.css')
+    .pipe(concat('style.min.css'))
+    .pipe(stripCssComments({all: true}))
+    .pipe(cssmin())
+    .pipe(gulp.dest('assets/css/'))
+});
